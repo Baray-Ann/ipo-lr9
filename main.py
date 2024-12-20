85c31d3d44a3d1250895c3479ea94a05d1aaf1e7
Baray Ann <annbaray66@gmail.com>
2024-12-20 22:52:20 +0300

    Update main.py

--- main.py
+++ main.py
@@ -9,13 +9,13 @@ print("Примеры isCollisionRect:")
 
 print(isCollisionRect([(-3.4, 1),(9.2, 10)], [(-7.4, 0),(13.2, 12)])) 
 print(isCollisionRect([(1, 1),(2, 2)], [(3, 0),(13, 1)]))               
-print(isCollisionRect([(1, 1),(2, 2)], [(3, 17),(13, 1)]))                
+print(isCollisionRect([(1, 1),(2, 2)], [(3, 17),(13, 1)])) #Пример вызывающий ошибку                
     
 print("Примеры intersectionAreaRect:")
 
-print(intersectionAreaRect([[(-3, 1), (9, 10)], [(-7, 0), (13, 12)]]))  
+print(intersectionAreaRect([[(-3, 1), (9, 10)], [(-7, 0), (13, 12)]])) #Пример вызывающий ошибку
 print(intersectionAreaRect([(1, 1), (2, 2)], [(3, 0), (13, 1)]))
-print(intersectionAreaRect([(1, 1), (2, 2)], [(3, 17), (13, 1)]))       
+print(intersectionAreaRect([(1, 1), (2, 2)], [(3, 17), (13, 1)])) #Пример вызывающий ошибку
    
 print("Примеры intersectionAreaMultiRect:")
 
@@ -28,6 +28,7 @@ rectangles = [
 
 print(f"Общая площадь пересечения: {intersectionAreaMultiRect(rectangles)}")
 
+#Пример вызывающий ошибку
 incorrect_rectangles = [
     [(-3, 1), (9, 10)],
     [(3, 17), (13, 1)]  
